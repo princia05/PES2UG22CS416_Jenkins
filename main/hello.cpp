37b817c9681c55c6f62d79c0bf1eed34c60238ff
unknown <princiadsouza05@gmail.com>
2025-03-14 15:25:12 +0530

    updated cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,11 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
-  cout << "Hello, World\n";
+int main(void)
+{
+  cout << "Hello, Princia\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "It hurts!! \n";
   return 0;
 }
